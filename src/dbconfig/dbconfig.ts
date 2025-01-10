a0a75020a7dec32b2228d31e90f7462eeebe60f5
stude-md <studymaewq@gmail.com>
2025-01-10 22:10:08 +0530

    first commit

--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -1,31 +1,31 @@
-import { MongoClient, Db } from "mongodb";
+// import { MongoClient, Db } from "mongodb";
 
-const uri: string | undefined = process.env.MONGODB_URI; // MongoDB connection string
-const options = {};
+// const uri: string | undefined = process.env.MONGODB_URI; // MongoDB connection string
+// const options = {};
 
-if (!uri) {
-  throw new Error("Please add your MongoDB URI to .env.local");
-}
+// if (!uri) {
+//   throw new Error("Please add your MongoDB URI to .env.local");
+// }
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
+// let client: MongoClient;
+// let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === "development") {
-  // In development, use a global variable to preserve the client across hot reloads
-  if (!(global as any)._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    (global as any)._mongoClientPromise = client.connect();
-  }
-  clientPromise = (global as any)._mongoClientPromise;
-} else {
-  // In production, always create a new connection
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-}
+// if (process.env.NODE_ENV === "development") {
+//   // In development, use a global variable to preserve the client across hot reloads
+//   if (!(global as any)._mongoClientPromise) {
+//     client = new MongoClient(uri, options);
+//     (global as any)._mongoClientPromise = client.connect();
+//   }
+//   clientPromise = (global as any)._mongoClientPromise;
+// } else {
+//   // In production, always create a new connection
+//   client = new MongoClient(uri, options);
+//   clientPromise = client.connect();
+// }
 
-export async function getDatabase(dbName: string): Promise<Db> {
-  const client = await clientPromise;
-  return client.db(dbName);
-}
+// export async function getDatabase(dbName: string): Promise<Db> {
+//   const client = await clientPromise;
+//   return client.db(dbName);
+// }
 
-export default clientPromise;
+// export default clientPromise;
